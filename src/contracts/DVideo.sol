b90c9f6e53b007f38825060e4c75bdcbd6d1ac46
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-25 00:58:13 +0300

    add requirement checks

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -16,6 +16,7 @@ contract DVideo {
     }
 
     //Create Event
+    event VideoUploaded(uint256 id, string hash, string title, address author);
 
     constructor() public {}
 
@@ -23,12 +24,16 @@ contract DVideo {
         public
     {
         // Make sure the video hash exists
+        require(bytes(_videoHash).length > 0);
         // Make sure video title exists
+        require(bytes(_title).length > 0);
         // Make sure uploader address exists
+        require(msg.sender != address(0));
         // Increment video id
         videoCount++;
         // Add video to the contract
         videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
         // Trigger an event
+        emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
     }
 }
