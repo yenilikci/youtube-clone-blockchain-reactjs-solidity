3737d34217d413757eb5deb093f2c9bc280b8f3f
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-25 00:25:03 +0300

    model the video, Video struct

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract DVideo {
-  uint public videoCount = 0;
-  string public name = "DVideo";
-  //Create id=>struct mapping
+    uint256 public videoCount = 0;
+    string public name = "DVideo";
+    //Create id=>struct mapping
 
-  //Create Struct
+    //1.Model the video
+    //Create Struct
+    struct Video {
+        uint256 id;
+        string hash;
+        string title;
+        address author;
+    }
 
+    //Create Event
 
-  //Create Event
+    constructor() public {}
 
-
-  constructor() public {
-  }
-
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
-
-    // Make sure video title exists
-
-    // Make sure uploader address exists
-
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
-  }
+    function uploadVideo(string memory _videoHash, string memory _title)
+        public
+    {
+        // Make sure the video hash exists
+        // Make sure video title exists
+        // Make sure uploader address exists
+        // Increment video id
+        // Add video to the contract
+        // Trigger an event
+    }
 }
