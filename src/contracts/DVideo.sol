64f3c42d0fd3b695f4af475518df05766e702605
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-25 00:33:59 +0300

    mapping videos and add video to the contract

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.5.0;
 contract DVideo {
     uint256 public videoCount = 0;
     string public name = "DVideo";
-    //Create id=>struct mapping
+
+    mapping(uint256 => Video) public videos;
 
     //1.Model the video
     //Create Struct
@@ -25,7 +26,9 @@ contract DVideo {
         // Make sure video title exists
         // Make sure uploader address exists
         // Increment video id
+        videoCount++;
         // Add video to the contract
+        videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
         // Trigger an event
     }
 }
