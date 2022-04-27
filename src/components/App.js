a585602b273cd4cb9597ebacfbc786193e793940
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-27 22:47:11 +0300

    lesson complete, uploaded videos listed

--- src/components/App.js
+++ src/components/App.js
@@ -1,142 +1,137 @@
-import React, { Component } from "react";
-import DVideo from "../abis/DVideo.json";
-import Navbar from "./Navbar";
-import Main from "./Main";
-import Web3 from "web3";
-import "./App.css";
+import React, {Component} from 'react';
+import DVideo from '../abis/DVideo.json'
+import Navbar from './Navbar'
+import Main from './Main'
+import Web3 from 'web3';
+import './App.css';
 
 //Declare IPFS
-const ipfsClient = require("ipfs-http-client");
-const ipfs = ipfsClient({
-  host: "ipfs.infura.io",
-  port: 5001,
-  protocol: "https",
-}); // leaving out the arguments will default to these values
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({host: 'ipfs.infura.io', port: 5001, protocol: 'https'}) // leaving out the arguments will default to these values
 
 class App extends Component {
+
   async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
+    await this.loadWeb3()
+    await this.loadBlockchainData()
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
     } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
+      window.web3 = new Web3(window.web3.currentProvider)
     } else {
-      window.alert(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3;
-    //Load accounts
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
-    //Add first account the the state
-    this.setState({ account: accounts[0] });
-
-    //Get network ID
-    const networkId = await web3.eth.net.getId();
-    //Get network data
-    const networkData = DVideo.networks[networkId];
-    //Check if net data exists, then
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = DVideo.networks[networkId]
     if (networkData) {
-      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address);
-      this.setState({ dvideo });
-
-      const videosCount = await dvideo.methods.videoCount().call();
-      this.setState({ videosCount });
+      const dvideo = new web3.eth.Contract(DVideo.abi, networkData.address)
+      this.setState({dvideo})
+      const videosCount = await dvideo.methods.videoCount().call()
+      this.setState({videosCount})
 
       // Load videos, sort by newest
       for (var i = videosCount; i >= 1; i--) {
-        const video = await dvideo.methods.videos(i).call();
+        const video = await dvideo.methods.videos(i).call()
         this.setState({
-          videos: [...this.state.videos, video],
-        });
+          videos: [...this.state.videos, video]
+        })
       }
 
       //Set latest video with title to view as default
-      const latest = await dvideo.methods.videos(videosCount).call();
+      const latest = await dvideo.methods.videos(videosCount).call()
       this.setState({
         currentHash: latest.hash,
-        currentTitle: latest.title,
-      });
-      this.setState({ loading: false });
+        currentTitle: latest.title
+      })
+      this.setState({loading: false})
     } else {
-      window.alert("DVideo contract not deployed to detected network");
+      window.alert('DVideo contract not deployed to detected network.')
     }
-    //Assign dvideo contract to a variable
-    //Add dvideo to the state
-
-    //Check videoAmounts
-    //Add videAmounts to the state
-
-    //Iterate throught videos and add them to the state (by newest)
-
-    //Set latest video and it's title to view as default
-    //Set loading state to false
-
-    //If network data doesn't exisits, log error
   }
 
-  //Get video
-  captureFile = (event) => {
-    event.preventDefault();
-    const file = event.target.files[0];
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(file);
+  captureFile = event => {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
 
     reader.onloadend = () => {
-      this.setState({ buffer: Buffer(reader.result) });
-      console.log("buffer", this.state.buffer);
-    };
-  };
+      this.setState({buffer: Buffer(reader.result)})
+      console.log('buffer', this.state.buffer)
+    }
+  }
 
-  //Upload video
-  uploadVideo = (title) => {};
 
-  //Change Video
-  changeVideo = (hash, title) => {};
+  uploadVideo = title => {
+    console.log("Submitting file to IPFS...")
+
+    //adding file to the IPFS
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result)
+      if (error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({loading: true})
+      this.state.dvideo.methods.uploadVideo(result[0].hash, title).send({from: this.state.account}).on('transactionHash', (hash) => {
+        this.setState({loading: false})
+      })
+    })
+  }
+
+  changeVideo = (hash, title) => {
+    this.setState({'currentHash': hash});
+    this.setState({'currentTitle': title});
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      loading: false,
-      account: "",
-      //set states
+      buffer: null,
+      account: '',
       dvideo: null,
       videos: [],
       loading: true,
       currentHash: null,
-      currentTitle: null,
-    };
+      currentTitle: null
+    }
 
-    //Bind functions
+    this.uploadVideo = this.uploadVideo.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+    this.changeVideo = this.changeVideo.bind(this)
   }
 
   render() {
     return (
-      <div>
-        <Navbar
-          //Account
-          account={this.state.account}
-        />
-        {this.state.loading ? (
-          <div id="loader" className="text-center mt-5">
-            <p>Loading...</p>
-          </div>
-        ) : (
-          <Main
-            //states&functions
-            captureFile={this.captureFile}
+        <div>
+          <Navbar
+              account={this.state.account}
           />
-        )}
-      </div>
+          {this.state.loading
+              ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+              : <Main
+                  videos={this.state.videos}
+                  uploadVideo={this.uploadVideo}
+                  captureFile={this.captureFile}
+                  changeVideo={this.changeVideo}
+                  currentHash={this.state.currentHash}
+                  currentTitle={this.state.currentTitle}
+              />
+          }
+        </div>
     );
   }
 }
